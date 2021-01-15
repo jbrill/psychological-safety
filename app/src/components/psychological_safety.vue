68dae199530d4ebe5b4756ffa0119c2b19674803
Jason Brill <jbrill95@gmail.com>
2021-01-15 11:03:55 -0500

    add button to psych safety page

--- app/src/components/psychological_safety.vue
+++ app/src/components/psychological_safety.vue
@@ -1,6 +1,9 @@
 <template>
   <v-container>
-    <h2>You've reached psychological safety!</h2>
+    <span style="padding-bottom: 2rem">You've reached psychological safety!</span>
+    <v-flex class="flex-column" align-items-center>
+      <v-btn color="black" class="white--text" to="/">Try again!</v-btn>
+    </v-flex>
   </v-container>
 </template>
 
